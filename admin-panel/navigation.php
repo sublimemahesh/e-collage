52005a981965aebd40e350ec9ac012b0af2f574e
ADinushika <amalidinushika93@gmail.com>
2020-02-19 17:03:21 +0530

    #41 Add admin panel git

---
+++ admin-panel/navigation.php
@@ -0,0 +1,37 @@
+<div class="layout-sidebar">
+    <div class="layout-sidebar-backdrop"></div>
+    <div class="layout-sidebar-body">
+        <div class="custom-scrollbar">
+            <nav id="sidenav" class="sidenav-collapse collapse">
+                <ul class="sidenav level-1">
+                    <li class="sidenav-heading">Navigation</li>
+
+                    <li class="sidenav-item has-subnav">
+                        <a href="#" aria-haspopup="true">
+                            <span class="sidenav-icon icon icon-works">p</span>
+                            <span class="sidenav-label">Student</span>
+                        </a>
+                        <ul class="sidenav level-2 collapse">
+                            <li class="sidenav-heading">Students</li>
+                            <li><a href="active-student.php">Active Student</a></li>
+                            <li><a href="inactive-student.php">InActive Student</a></li>
+                            <li><a href="manage-all-students.php">Manage All Student</a></li>
+                        </ul>
+                    </li>
+<!--                    <li class="sidenav-item has-subnav">
+                        <a href="#" aria-haspopup="true">
+                            <span class="sidenav-icon icon icon-works">a</span>
+                            <span class="sidenav-label">Course Content</span>
+                        </a>
+                        <ul class="sidenav level-2 collapse">
+                            <li class="sidenav-heading">Course Content</li>
+                            <li><a href="content-category.php">Beginner</a></li>
+                            <li><a href="content-category.php">Intermediate</a></li>
+                            <li><a href="content-category.php">Advanced</a></li>
+                        </ul>
+                    </li>-->
+                </ul>
+            </nav>
+        </div>
+    </div>
+</div>
