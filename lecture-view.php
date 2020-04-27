7fa6035a1d60766d46c96a82596243d053a52971
Tharushi07 <tharushi.synotec@gmail.com>
2020-04-27 18:12:22 +0530

    #76 Create lecture-view page

--- lecture-view.php
+++ lecture-view.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 
-<!-- Mirrored from kl-webmedia.com/demo/bromine/teacher-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Apr 2020 06:52:12 GMT -->
+<!-- Mirrored from kl-webmedia.com/demo/bromine/teacher-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 27 Apr 2020 06:58:49 GMT -->
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<title>Bromine | Kevin Micfield</title>
+	<title>ecollege.lk | View Lecture</title>
 	<link rel="stylesheet" href="assets/libs/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/libs/material-design-iconic-font/css/material-design-iconic-font.min.css">
 	<link rel="stylesheet" href="assets/libs/jquery-ui/jquery-ui.min.css">
@@ -17,13 +17,92 @@
 
 <body id="teacher-single" class="page">
 
- 
-<main>
- 
+<?php include './header-2.php'; ?>
 
-	
+<main>
+	<div class="container teacher-page">
+		<div class="row">
+			<div class="col-sm-3 text-center">
+				<img src="assets/images/teacher.png" alt="Avatar" class="teacher-avatar">
+				<ul class="social">
+					<li><a href="#"><i class="zmdi zmdi-facebook"></i></a></li><li>
+					<a href="#"><i class="zmdi zmdi-twitter"></i></a></li><li>
+					<a href="#"><i class="zmdi zmdi-google-old"></i></a></li><li>
+					<a href="#"><i class="zmdi zmdi-pinterest"></i></a></li><li>
+					<a href="#"><i class="zmdi zmdi-instagram"></i></a></li><li>
+					<a href="#"><i class="zmdi zmdi-email"></i></a></li><li>
+					<a href="#"><i class="zmdi zmdi-globe-alt"></i></a></li>
+				</ul>
+			</div>
+			<div class="col-sm-9">
+				<h5>Kevin Micfield</h5>
+				<p>SOFTWARE ENGINEER AND CONSULTANT</p>
+				<p class="class-details">	
+					<span class="lessons"><i class="zmdi zmdi-assignment"></i>8 COURSES</span>
+					<span class="views"><i class="zmdi zmdi-eye"></i>2241 VIEWS</span>
+					<span class="rating"><i class="zmdi zmdi-star"></i>AVERAGE RATES : 4,8</span>
+				</p>
+				<p class="abs">
+					Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore 
+					magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
+					gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing 
+					elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos 
+					et accusam et justo duo dolores et ea rebum. 
+				</p>
+				<p class="abs">
+					Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, 
+					consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam 
+					voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus 
+					est Lorem ipsum dolor sit amet.   
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<div class="container popular">
+		<h4>Kevin Micfield Courses</h4>
+		<div class="row tutorials">
+			<div class="tutorial">
+				<img src="assets/images/popular/1.jpg" class="resp-img" alt="Tutorial">
+				<div class="tutorial-details">
+					<h6>How to become an UX Designer</h6>
+					<p><span class="lessons"><i class="zmdi zmdi-assignment"></i>12 LESSONS</span><span class="duration"><i class="zmdi zmdi-time"></i>3:15h</span></p>
+					<p class="abs">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+					<a href="#" class="greybutton">VIEW COURSE</a>
+				</div>
+			</div>
+			<div class="tutorial">
+				<img src="assets/images/popular/2.jpg" class="resp-img" alt="Tutorial">
+				<div class="tutorial-details">
+					<h6>8 steps to your own startup</h6>
+					<p><span class="lessons"><i class="zmdi zmdi-assignment"></i>8 LESSONS</span><span class="duration"><i class="zmdi zmdi-time"></i>4:32h</span></p>
+					<p class="abs">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+					<a href="#" class="greybutton">VIEW COURSE</a>
+				</div>
+			</div>
+			<div class="tutorial">
+				<img src="assets/images/popular/3.jpg" class="resp-img" alt="Tutorial">
+				<div class="tutorial-details">
+					<h6>Business communication for marketers</h6>
+					<p><span class="lessons"><i class="zmdi zmdi-assignment"></i>15 LESSONS</span><span class="duration"><i class="zmdi zmdi-time"></i>6:04h</span></p>
+					<p class="abs">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+					<a href="#" class="greybutton">VIEW COURSE</a>
+				</div>
+			</div>
+			<div class="tutorial">
+				<img src="assets/images/popular/2.jpg" class="resp-img" alt="Tutorial">
+				<div class="tutorial-details">
+					<h6>8 steps to your own startup</h6>
+					<p><span class="lessons"><i class="zmdi zmdi-assignment"></i>8 LESSONS</span><span class="duration"><i class="zmdi zmdi-time"></i>4:32h</span></p>
+					<p class="abs">At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est.</p>
+					<a href="#" class="greybutton">VIEW COURSE</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </main>
- 
+
+<?php include './footer.php'; ?>
 
 <script src="assets/libs/jquery/jquery.js"></script>
 <script src="assets/libs/bootstrap/js/bootstrap.min.js"></script>
@@ -33,5 +112,5 @@
 <script src="assets/js/main.js"></script>
 </body>
 
-<!-- Mirrored from kl-webmedia.com/demo/bromine/teacher-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 20 Apr 2020 06:52:14 GMT -->
+<!-- Mirrored from kl-webmedia.com/demo/bromine/teacher-single.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 27 Apr 2020 06:58:51 GMT -->
 </html>
