3c80ad5c706d02ed21e0ee9d3e7de2cdd0d41188
ADinushika <amalidinushika93@gmail.com>
2020-05-11 11:53:55 +0530

    #130 Agreement Correction

--- lecture/ajax/js/registration.js
+++ lecture/ajax/js/registration.js
@@ -62,43 +62,53 @@ $(document).ready(function () {
             }
         }
     });
-
     form.keypress(300);
+    $('#next').click(function () {
 
-    $('button[type="submit"]').click(function () {
-        if ($('#agreement').prop("checked") == false) {
-            swal({
-                title: "Error!",
-                text: "Please accept our term and conditions",
-                type: 'error',
-                timer: 2000,
-                showConfirmButton: false
+        form.test();
+        if (form.errors() == 0) {
+            $('#agreement_form').show();
+            $('#register_form').hide();
+
+
+            $('#black').click(function () {
+                $('#register_form').show();
+                $('#agreement_form').hide();
+
             });
-        } else {
-            form.test();
-            if (form.errors() == 0) {
-                var formData = new FormData($("form#form")[0]);
-                $.ajax({
-                    url: "ajax/post-and-get/registration.php",
-                    type: 'POST',
-                    data: formData,
-                    async: false,
-                    cache: false,
-                    contentType: false,
-                    processData: false,
-                    dataType: "JSON",
-                    success: function (result) {
-                        if (result.status == 'error') {
-                            $('#message').text(result.message);
-                        } else {
-                            window.location.replace("index.php");
+            $('#register').click(function () {
+                if ($('#agreement').prop("checked") == false) {
+                    swal({
+                        title: "Error!",
+                        text: "Please accept our term and conditions",
+                        type: 'error',
+                        timer: 2000,
+                        showConfirmButton: false
+                    });
+                } else {
+                    var formData = new FormData($("form#form")[0]);
+                    $.ajax({
+                        url: "ajax/post-and-get/registration.php",
+                        type: 'POST',
+                        data: formData,
+                        async: false,
+                        cache: false,
+                        contentType: false,
+                        processData: false,
+                        dataType: "JSON",
+                        success: function (result) {
+                            if (result.status == 'error') {
+                                $('#message').text(result.message);
+                            } else {
+                                window.location.replace("index.php");
+                            }
                         }
-                    }
-                });
-            }
-            return false;
+                    });
+                }
+            });
+
         }
+        return false;
+
     });
-
-
 });
