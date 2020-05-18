e325b03e06f250ff27952eb27ce559acd0653cdb
ADinushika <amalidinushika93@gmail.com>
2020-05-18 11:43:28 +0530

    #158 Student Dashboard Correction

--- student/ajax/js/student_class_registration.js
+++ student/ajax/js/student_class_registration.js
@@ -1,23 +1,69 @@
 
 $(document).ready(function () {
-    
-    
-    $(".select").click(function (event) {
+
+//add selected classes
+    $('.card-body').on('click', '.select', function (event) {
         event.preventDefault();
+
+        var class_id = $(this).attr("data-id");
+        var stu_id = $(this).attr("stu_id");
+        var lecture_id = $(this).attr("lecture_id");
  
-            var formData = new FormData($('.form-data')[0]);
+        $.ajax({
+            url: "ajax/post-and-get/student_class_registration.php",
+            type: "POST",
+            data: {
+                class_id: class_id,
+                stu_id: stu_id,
+                lecture_id: lecture_id,
+                option: 'create'
+            },
+            dataType: "JSON",
+            success: function (result) {
+
+                swal({
+                    title: "Success!",
+                    text: "Your are added class saved successfully!.....",
+                    type: 'success',
+                    timer: 2000,
+                    showConfirmButton: false
+                }, function () {
+                    setTimeout(function () {
+                        window.location.replace("select-class.php");
+                    }, 1500);
+                });
+            },
+        });
+    });
+
+
+
+    $('.card-body').on('click', '.remove', function (event) {
+        event.preventDefault();
+
+        var id = $(this).attr("data-id");
+      
+        swal({
+            title: "Are you sure?",
+            text: "Now you are leave this class Room..!",
+            type: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#DD6B55",
+            confirmButtonText: "Yes, leave now.!",
+            closeOnConfirm: false
+        }, function () {
 
             $.ajax({
-                  url: "ajax/post-and-get/student_class_registration.php",
+                url: "ajax/post-and-get/student_class_registration.php",
                 type: "POST",
-                data: formData,
-                async: false,
-                dataType: 'json',
-                success: function (result) {
-                  
+                data: {id: id, option: 'delete'},
+                dataType: "JSON",
+                success: function (jsonStr) {
+
+                    if (jsonStr.status) {
                         swal({
-                            title: "Success!",
-                            text: "Your data was saved successfully!.....",
+                            title: "Leave the Class!",
+                            text: "You are leave this class Room.",
                             type: 'success',
                             timer: 2000,
                             showConfirmButton: false
@@ -26,15 +72,14 @@ $(document).ready(function () {
                                 window.location.replace("select-class.php");
                             }, 1500);
                         });
-                   
-                },
-                cache: false,
-                contentType: false,
-                processData: false
+
+
+                    }
+                }
             });
-       
-    }); 
-    
-    
+        });
+    });
+
+
 });
 
